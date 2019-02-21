d65951a9884ceb06194a40e8537eb5b3d36bc6e9
Laurent <jeanson_laurent@outlook.com>
2019-02-21 00:53:55 -0500

    Creation de dossiers et commencement danimation POO

---
+++ module_js/AnimImg.js
@@ -0,0 +1,69 @@
+import {
+    Util
+} from './Util.js'
+
+
+export class AnimImg {
+
+    /**
+     * Classe permettant de créer et d'animer une introduction
+     * @param {string} lesImages - contient l'ensemble des mots d'intro
+     * @param {DOMElement} elementParent - Conteneur de l'animation
+     * @param {function} fonction - l'adresse de la fonction à exécuter après l'animation
+         
+     }}
+     */
+
+    constructor(lesImages, elementParent, fonction) {
+        //Récupérer les valeurs passées en paramètre			
+        this.lesImages = lesImages
+        this.elmParent = elementParent
+        this.AnimImg(this.lesImages)
+        this.fonction = fonction
+    }
+
+
+    lesImages(lesImages) {
+        /* Création des élément DOM qui seront animés. 
+        Les éléments seront intégré dans le conteneur elmParent
+        */
+       let i = 0;
+       const tabCouleur = ['#fed136', '#BFB000' , '#FFEA00', '#403B00', '#E5D300','#fed136']
+       let nbLettres = lesLettres.length;
+        let elmConteneur = this.creerElement(this.elmParent,
+            'section',
+            '',
+            'mot')
+            
+        for (let uneLettre of lesLettres){
+            let elmLettre = this.creerElement(elmConteneur, 'div', uneLettre, 'mot')
+            elmLettre.style.animationDelay = (i * 0.5) + "s";
+            elmLettre.style.color = tabCouleur[(i++)%5]    
+        }
+
+        /* On garde une référence sur la fonction terminerIntro */
+        let refTerminerIntro = this.terminerIntro.bind(this)
+    }
+
+    creerElement(elmParent, balise, contenu, classCSS) {
+        console.log(balise)
+        let noeud = document.createElement(balise)
+        if (contenu != '') {
+            noeud.innerHTML = contenu
+        }
+        noeud.classList.add(classCSS)
+        elmParent.appendChild(noeud)
+        return noeud
+    }
+
+    terminerIntro(evt) {
+        this.elmParent.firstChild.classList.add('deplacementContenuIntro')
+        this.elmParent.firstChild.addEventListener('animationend', this.passerVersAnimationSuivante.bind(this))
+    }
+
+    passerVersAnimationSuivante(evt) {
+        Util.detruireTousLesNoeud(this.elmParent, this.elmParent)
+        this.fonction()
+    }
+
+}
--- module_js/application.js
+++ module_js/application.js
@@ -2,11 +2,12 @@
 import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
 import {Introduction} from './Introduction.js' // 
 import {AnimLettre} from './AnimLettre.js' //
+import {AnimImg} from './AnimImg.js' //
 
 
 /* l'élement de la page qui contiendra les éléments créés dynamiquement */
 let elmHeader = document.querySelector('.header')
-let intro = new Introduction(contenuIntro, elmHeader, animationLettre, animationLettreImg)
+let intro = new Introduction(contenuIntro, elmHeader, animationLettre, animationImage)
 
 // debutQuestionnaire()
 
@@ -17,12 +18,12 @@ function animationLettre()
 	const lesLettres = 'Section-image'
 	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim)	
 }
-function animationLettreImg()
+function animationImage()
 {
     /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */ 
 	console.log('debut animation lettre Img')
-	const lesLettresImg = 'Section-Image'
-	let  monAnimLettreImg = new AnimLettre(lesLettresImg)	
+	const lesImages = 'Section-Image'
+	let  monAnimImg = new AnimImg(lesImages)	
 }
 
 function finAnim()
